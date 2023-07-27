5d6958ba5452fb9640814e624511dd24a05a6557
Adharsh <67263152+adh-arsh@users.noreply.github.com>
2023-07-27 23:11:48 +0530

    init

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 #pip install beautifulsoup4
 #pip install selenium
 
+#https://stackoverflow.com/questions/13166395/fill-input-of-type-text-and-press-submit-using-python
+
+
+
+
+#https://www.bseindia.com/markets/equity/EQReports/StockPrcHistori.html?flag=0
+
+#use beautifulsoup library to populate text fields and click submit button
+
+
+#search_in_page(link = "https://www.bseindia.com/markets/equity/EQReports/StockPrcHistori.html?flag=0", companyCode = "BSECODE", radio="daily", from="01/07/2023" , to="10/07/2023")
+
+
+
+
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+#how to populate textfields beautifulsoup
+#how to populate radiobutton beautiful soup
+#how to press button beautiful soup
+
+
+
 # For Chrome
 driver = webdriver.Chrome()
 
-url = "https://example.com"  # Replace with the URL of the web page you want to scrape
+url = "https://www.bseindia.com/markets/equity/EQReports/StockPrcHistori.html?fl"  # Replace with the URL of the web page you want to scrape
 driver.get(url)
 
 # Wait for the element to be loaded (optional)
